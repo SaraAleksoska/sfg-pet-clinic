54b0224364e69f86b058b0f0b875077a2dac664f
SaraAleksoska <72557477+SaraAleksoska@users.noreply.github.com>
2021-11-03 14:14:15 +0100

    Add files via upload

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Person.java
@@ -0,0 +1,22 @@
+package guru.springframework.sfgpetclinic.model;
+
+public class Person {
+    private String firstName;
+    private String lastNAme;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastNAme() {
+        return lastNAme;
+    }
+
+    public void setLastNAme(String lastNAme) {
+        this.lastNAme = lastNAme;
+    }
+}
