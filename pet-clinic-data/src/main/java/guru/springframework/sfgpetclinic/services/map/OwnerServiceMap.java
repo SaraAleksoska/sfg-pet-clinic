f263d82db66b8210c770c4a6238acdd543a5953c
SaraAleksoska <72557477+SaraAleksoska@users.noreply.github.com>
2021-11-04 23:13:41 +0100

    Add files via upload

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/OwnerServiceMap.java
@@ -2,37 +2,27 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 import guru.springframework.sfgpetclinic.services.OwnerService;
+import org.springframework.stereotype.Service;
 
 import java.util.Set;
 
+@Service
 public class OwnerServiceMap extends AbstractMapService<Owner, Long> implements OwnerService {
     @Override
-    public Set<Owner> findAll() {
-        return super.findAll();
-    }
+    public Set<Owner> findAll() {return super.findAll();}
 
     @Override
-    public void deleteByID(Long id) {
-        super.deleteByID(id);
-    }
+    public void deleteByID(Long id) {super.deleteByID(id);}
 
     @Override
-    public void delete(Owner object) {
-        super.delete(object);
-    }
+    public void delete(Owner object) {super.delete(object);}
 
     @Override
-    public Owner save(Owner object) {
-        return super.save(object.getId(), object);
-    }
+    public Owner save(Owner object) {return super.save(object.getId(), object);}
 
     @Override
-    public Owner findByID(Long id) {
-        return super.findByID(id);
-    }
+    public Owner findByID(Long id) {return super.findByID(id);}
 
     @Override
-    public Owner findByLastName(String LastName) {
-        return null;
-    }
+    public Owner findByLastName(String LastName) {return null;}
 }
