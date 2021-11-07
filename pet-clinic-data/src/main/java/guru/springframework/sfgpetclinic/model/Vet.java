4d8c9fd74941d685a5324ea9c115c70717ace32e
SaraAleksoska <72557477+SaraAleksoska@users.noreply.github.com>
2021-11-07 20:10:53 +0100

    Add files via upload

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Vet.java
@@ -1,17 +1,18 @@
 package guru.springframework.sfgpetclinic.model;
 
+import java.util.HashSet;
 import java.util.Set;
 
 public class Vet extends Person{
 
-    private Set<Speciality> getSpecialities;
+    private Set<Speciality> specialities = new HashSet<>();
 
-    public Set<Speciality> getGetSpecialities() {
-        return getSpecialities;
+    public Set<Speciality> getSpecialities() {
+        return specialities;
     }
 
-    public void setGetSpecialities(Set<Speciality> getSpecialities) {
-        this.getSpecialities = getSpecialities;
+    public void setSpecialities(Set<Speciality> specialities) {
+        this.specialities = specialities;
     }
 }
 
