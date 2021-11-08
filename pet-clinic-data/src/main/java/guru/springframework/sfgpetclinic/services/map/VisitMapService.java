10b0ab6db69a9b73031a61cb3c4d7640f0e4bbe2
SaraAleksoska <72557477+SaraAleksoska@users.noreply.github.com>
2021-11-08 16:42:51 +0100

    Add files via upload

---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitMapService.java
@@ -0,0 +1,43 @@
+package guru.springframework.sfgpetclinic.services.map;
+
+import guru.springframework.sfgpetclinic.model.Visit;
+import guru.springframework.sfgpetclinic.services.VetServices;
+import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+import java.util.Set;
+
+@Service
+@Profile({"default", "map"})
+public class VisitMapService extends AbstractMapService<Visit, Long> implements VisitService {
+
+    @Override
+    public Set<Visit> findAll() {
+        return super.findAll();
+    }
+
+    @Override
+    public void deleteByID(Long id) {
+        super.deleteByID(id);
+    }
+
+    @Override
+    public void delete(Visit object) {
+        super.delete(object);
+    }
+
+    @Override
+    public Visit save(Visit visit) {
+        if(visit.getPet() == null || visit.getPet().getOwner() == null || visit.getPet().getId() == null
+        || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalide visit");
+        }
+        return super.save(visit);
+    }
+
+    @Override
+    public Visit findByID(Long id) {
+        return super.findByID(id);
+    }
+}
